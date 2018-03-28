a71090199e0eb60b5c43f18acb9edfa6d952f1a8
Luca-Leon Happel <lucahappel99@gmx.de>
2018-03-28 15:45:26 +0200

    Update cutImg.js

--- cutImg.js
+++ cutImg.js
@@ -1,3 +1,9 @@
+// ### CUTIMG library ###
+// @author Luca Leon Happel 2017
+//
+// example:
+// newImage = cutImg(originalImage, 100, 200, 450, 600);
+//
 // create a new canvas, in which all the images are cutted to pieces in
 converterCanvas = document.createElement("canvas");
 // make this canvas invisible, so it does not distract the user
@@ -5,19 +11,19 @@ converterCanvas.style.display = "none";
 
 // this function takes in an image, then cuts it into a portion of the previous image and returns a new image
 function cutImg(image, x, y, width, height) {
-    converterCanvas.width = width;
-    converterCanvas.height = height;
-    converterCanvas.getContext("2d").drawImage(image, -1 * x, -1 * y);
+converterCanvas.width = width;
+converterCanvas.height = height;
+converterCanvas.getContext("2d").drawImage(image, -1 * x, -1 * y);
 
-    var output = new Image();
-	output.src = converterCanvas.toDataURL("image/png");
+var output = new Image();
+output.src = converterCanvas.toDataURL("image/png");
 
-    return output;
+return output;
 }
 // this function takes in an image and replicates it
 function copyImg(canvas) {
-    var output = new Image();
-    output.src = canvas.toDataURL("image/png");
+var output = new Image();
+output.src = canvas.toDataURL("image/png");
 
-    return output;
+return output;
 }

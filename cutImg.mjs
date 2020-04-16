d4f2256a53c3fb186f58a894309016dfc15b6764
Quoteme <lucahappel99@gmx.de>
2020-04-16 04:40:39 +0200

    added mirror option

--- cutImg.mjs
+++ cutImg.mjs
@@ -3,19 +3,14 @@
 // AUTHOR: Luca Leon Happel
 // DATE: 19.10.2019
 
-export{
-	cut,
-	split,
-	join
-}
-
 let c = document.createElement("canvas");
 c.style.display = "none";
 
 let ctx = c.getContext("2d")
 
+// cut an image
 // cut :: Image -> Float -> Float -> Float -> Float -> Image
-function cut(image, x, y, width, height){
+export function cut(image, x, y, width, height){
 	c.width = width;
 	c.height = height;
 	ctx.drawImage(image, -1 * x, -1 * y);
@@ -24,13 +19,14 @@ function cut(image, x, y, width, height){
 	return out;
 }
 
+// split an image into tiles of specific width and height
 // split :: Image -> Float -> Float -> [[Image]]
-function split(image, tileWidth, tileHeight){
+export function split(image, tileWidth, tileHeight){
 	return new Array(Math.floor(image.height/tileHeight))
 		.fill(0)
-		.map((a,y) => new Array(Math.floor(image.width/tileWidth))
+		.map((_,y) => new Array(Math.floor(image.width/tileWidth))
 			.fill(0)
-			.map((b,x) => cut(
+			.map((_,x) => cut(
 				image,
 				x*tileWidth,
 				y*tileHeight,
@@ -39,12 +35,28 @@ function split(image, tileWidth, tileHeight){
 			)))
 }
 
+// join two images together (2nd image is offset by x and y)
 // join :: Img -> Img -> Int -> Int -> Img
-function join(a,b,x,y){
+export function join(a,b,x,y){
 	c.width = a.width + b.width + Math.abs( x-a.width );
 	c.height = a.height + b.height + Math.abs( y-a.height );
 	ctx.drawImage(a,0,0);
 	ctx.drawImage(b,x,y);
+	let out = new Image();
+		out.src = c.toDataURL("image/png");
+	return out;
+}
+
+// mirror an image along one or two specified axes
+// mirror :: Img -> Bool -> Bool -> Img
+export function mirror(image, x=true,y=false){
+	c.width = image.width;
+	c.height = image.height;
+	ctx.scale(
+		x?-1:1,
+		y?-1:1,
+	)
+	ctx.drawImage(image,0,0);
 	let out = new Image();
 		out.src = c.toDataURL("image/png");
 	return out;
